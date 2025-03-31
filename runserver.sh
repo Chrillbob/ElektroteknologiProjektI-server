eb033c7d9fa987f26e9f3209efd54d719a494b1a
chrillbob <chrissenjesper@gmail.com>
2025-03-31 10:31:49 +0200

    Fixed errors in startup script and compose

--- runserver.sh
+++ runserver.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-python3 ./WSApp/manage.py makemigrations WeatherStation
-python3 ./WSApp/manage.py migrate
-python3 ./WSApp/manage.py runmodwsgi --setup-only --port=80 --user www-data --group www-data --server-root=/etc/mod_wsgi-express-80
+python3 ./manage.py makemigrations WeatherStation
+python3 ./manage.py migrate
+python3 ./manage.py runmodwsgi --setup-only --port=80 --user www-data --group www-data --server-root=/etc/mod_wsgi-express-80
 /etc/mod_wsgi-express-80/apachectl start --log-to-terminal -D FOREGROUND
