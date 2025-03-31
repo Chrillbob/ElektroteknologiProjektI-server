6f3b215af16f967050f3d48f7dc1f9a6885440f0
chrillbob <chrissenjesper@gmail.com>
2025-03-31 09:45:42 +0200

    Fixed error in startup script

--- runserver.sh
+++ runserver.sh
@@ -1,3 +1,3 @@
 python3 ./WSApp/manage.py makemigrations WeatherStation
 python3 ./WSApp/manage.py migrate
-/etc/mod_wsgi-express-80/apachectl", "start", "--log-to-terminal", "-D", "FOREGROUND"
+/etc/mod_wsgi-express-80/apachectl start --log-to-terminal -D FOREGROUND
