80af5131b9525225f5f7d8116d793cb824068a57
chrillbob <chrissenjesper@gmail.com>
2025-04-07 12:07:30 +0200

    Set timezone to Copenhagen

--- WeatherStation/models.py
+++ WeatherStation/models.py
@@ -12,7 +12,7 @@ class Measurements(models.Model):
     pressure = models.FloatField("air pressure")
     smoke = models.FloatField("smoke level")
     ambient = models.FloatField("ambient light level")
-    meas_time = models.DateTimeField("measurement time", default=timezone.localtime())
+    meas_time = models.DateTimeField("measurement time", default=timezone.localtime(timezone='Europe/Copenhagen'))
 
     def __str__(self):
         return "Measurement at " + self.meas_time.isoformat()
